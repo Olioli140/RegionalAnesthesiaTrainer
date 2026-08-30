f6625ab115073af77eb9b3073a822471659b537e
Olioli140 <oliolischmidt1991@gmx.de>
2026-08-30 08:59:28 +0200

    Atlas v1: mount navigation shell

--- apps/regional-anesthesia-trainer/src/main.tsx
+++ apps/regional-anesthesia-trainer/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import RootApp from './RootApp';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RootApp />
   </React.StrictMode>
 );
